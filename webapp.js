b40d62b818a2134f9f7e6fe40ff71c4c6527c03b
Nathan Pearce <nathan.pearce@warriorlife.net>
2018-01-29 10:46:05 -0800

    edited the js

--- webapp.js
+++ webapp.js
@@ -23,20 +23,20 @@ function updateScene(s) {
 			});
 			break;
 		case "s1":
-			document.getElementById("storyline").innerHTML = " <p>If player chooses yes</p><p>Base:Ok, try to keep it on.</p> <p>Harold:Ok</p><p>Base: Wow, it appears you are in the planet...";
+			document.getElementById("storyline").innerHTML = "<p>Base: Ok, try to keep it on.</p><p>Harold: Ok</p><p>Base: Wow, it appears you are in the planet...";
 			//change btn1
 			document.getElementById("btn1").innerHTML = "Ermina 3";
 			document.getElementById("btn1").addEventListener("click", function() {
 				updateScene("s2");
 			});
 			//change btn2
-			document.getElementById("btn2").innerHTML ="Sirona";
+			document.getElementById("btn2").innerHTML = "Sirona";
 			document.getElementById("btn2").addEventListener("click", function() {
 				updateScene("s5");
 			});
 			break;
 		case"s2":
-			document.getElementById("storyline").innerHTML ="<p>Planet:Ermina 3 <p>Harold: How many light years is that from Earth?</p><p>About seven light years away. This means it will take us apporximately  a week to get to your location.</p><p>Ok, I think I had enough supplies to survive, but I do need some isotope base 23 becasue my supply of it is running out.</p><p>Base: What would happen if you didn't have it?</p><p>HaroldIsotope base 23 is the thing that keeps me living. In your planet, it is referred to as food</p><p>Should I go find some more food or should I start making a shelter?</p>";
+			document.getElementById("storyline").innerHTML ="<p>Planet Ermina 3<p>Harold: How many light years is that from Earth?</p><p>Base: About seven light years away. This means it will take us apporximately a week to get to your location.</p><p>Harold: Ok, I think I had enough supplies to survive, but I do need some Isotope Base 23 becasue my supply of it is running out.</p><p>Base: What would happen if you didn't have it?</p><p>Harold: Isotope base 23 is the thing that keeps me living. Base: On your planet, it is referred to as food</p><p>Harold: Should I go find some more food or should I start making a shelter?</p>";
 			//change btn1
 			 document.getElementById("btn1").innerHTML = " Food";
 			document.getElementById("btn1").addEventListener("click", function() {
@@ -49,7 +49,7 @@ function updateScene(s) {
 			});
 			break;
 		case"s3":
-			document.getElementById("storyline").innerHTML ="<p> If you choose no </p> <p>Base: How many moons are there on that planet? How long is each day?</p><p>Harold: There are about 7 moons and it takes about 37 hours to get through the day. The planet has no living beings except for plants.</p><p>Base:How's your food supply</p><p>Harold:I am surviving only only on a few berries and mangoes right now, but I am starting to lose my supply of it.</p> <p>Harold:I'll get more food or I'll go creater a shelter</p>";
+			document.getElementById("storyline").innerHTML ="<p>Base: How many moons are there on that planet? How long is each day?</p><p>Harold: There are about seven moons and it takes about 37 hours to get through one day. The planet has no living beings except for some scattered plants.</p><p>Base: How's your food supply?</p><p>Harold: I am surviving only only on a few berries and mangoes right now, but I am starting to lose my supply of it.</p><p>Harold: I'll get more food or I'll go creater a shelter</p>";
 			//change btn1
 			document.getElementById("btn1").innerHTML = "Food";
 			document.getElementById("btn1").addEventListener("click", function() {
@@ -62,13 +62,13 @@ function updateScene(s) {
 			});
 			break;
 		case"s4":
-			document.getElementById("storyline").innerHTML ="<p>If you get more food:</p> <p>Base:Apparently, that planet has an abudnace of nagoes, berries, and fart causing oranges</p><p>Harold:Ok got it.</p><p>Base: We should be able to get there in about 5 earth days. <p>Harold:After I find more food, I will build a shelter.</p>";
+			document.getElementById("storyline").innerHTML ="<p>Base: Apparently, that planet has an abundace of nagoes, burp causing berries, and fart causing oranges</p><p>Harold: Ok, got it.</p><p>Base: We should be able to get there in about five Earth days.<p>Harold: After I find more food, I will build a shelter.</p>";
 			break;
 		case"s5":
-			document.getElementById("storyline").innerHTML ="<p>You build a shelter</p> <p>Harold: I'll go cut some wood and prepare for the night. At night, the winters go down to -4 degrees Fahrenheit.</p><p>Base: That planet has a lot of isotope base 189 which is an extremely good insulator.</p> <p>Harold: Got it</p><p> Base: Now you should go to sleep or you could stay awake and look out for more stuff during the night.</p>"; 
+			document.getElementById("storyline").innerHTML ="<p>Harold: I'll go cut some wood and prepare for the night. At night, the weather goes down to about -4 degrees Fahrenheit.</p><p>Base: That planet has a lot of Isotope Base 189 which is an extremely good insulator.</p><p>Harold: Got it</p><p>Base: Now you should go to sleep or you could stay awake and look out for more stuff during the night.</p>"; 
 			break;
 		case"s6":
-			document.getElementById("storyline").innerHTML=" <p>If you choose to sleep</p> Harold: Ok now it is the morning currently unaware of the time.</p><p>Base: I guess we know that it is the morning and that you got to start doing something.</p><p>Harold:Alright I guess so... Im going to go hunt for some meat now that I know that I have a steady flow of fruits.</p> <p>Base: All right, do you want to fish or hunt on land?</p>";
+			document.getElementById("storyline").innerHTML="Harold: Ok now it is the morning currently unaware of the time.</p><p>Base: I guess we know that it is the morning and that you got to start doing something.</p><p>Harold:Alright I guess so... I'm going to go hunt for some meat now that I know that I have a steady flow of fruits.</p><p>Base: All right, do you want to fish or hunt on land?</p>";
 			break;
 	}
  }
