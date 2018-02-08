075ca210785aa152d385814bebce23b21510a0eb
Roy Park <roy.park@warriorlife.net>
2018-02-08 10:54:26 -0800

    started editing..finish what i have started father

--- webapp.js
+++ webapp.js
@@ -13,100 +13,84 @@ function updateScene(s) {
 			document.getElementById("storyline").innerHTML = "<p>The year is 2753.</p><p>Harold: Is anyone out there? Hello?...</p><p>Base: Who is this?</p><p>Harold: My name is Captain Harold.</p><p>Base: Captain Harold! We thought you died when the Falcon exploded!</p><p>Harold: I was the only survivor. I am on an unknown planet with limited resources and my ship is in ruins.</p><p>Base: Ok, we will try to get to you. Do you still have your locator we gave you?</p>";
 			// change btn1
 			document.getElementById("btn1").innerHTML = "Yes";
-			document.getElementById("btn1").addEventListener("click", function() {
-				updateScene("s1");
-			});
+			document.getElementById("btn1").setAttribute("onclick", "updateScene('s1')");
 			// change btn2
 			document.getElementById("btn2").innerHTML = "No";
-			document.getElementById("btn2").addEventListener("click", function() {
-				updateScene("s3");
+			 document.getElementById("btn1").setAttribute("onclick", "updateScene('s3')");
 			});
 			break;
 		case "s1":
 			document.getElementById("storyline").innerHTML = "<p>Base: Ok, try to keep it on.</p><p>Harold: Ok</p><p>Base: Wow, it appears you are on the planet...";
 			//change btn1
 			document.getElementById("btn1").innerHTML = "Ermina 3";
-			document.getElementById("btn1").addEventListener("click", function() {
-				updateScene("s2");
+			 document.getElementById("btn1").setAttribute("onclick", "updateScene('s2')");
 			});
 			//change btn2
 			document.getElementById("btn2").innerHTML = "Sirona";
-			document.getElementById("btn2").addEventListener("click", function() {
-				updateScene("s5");
-			});
+			 document.getElementById("btn1").setAttribute("onclick", "updateScene('s5')");			
+			 });
 			break;
 		case"s2":
 			document.getElementById("storyline").innerHTML ="<p>Planet Ermina 3<p>Harold: How many light years is that from Earth?</p><p>Base: About seven light years away. This means it will take us apporximately a week to get to your location.</p><p>Harold: Ok, I think I had enough supplies to survive, but I do need some Isotope Base 23 becasue my supply of it is running out.</p><p>Base: What would happen if you didn't have it?</p><p>Harold: Isotope base 23 is the thing that keeps me living. Base: On your planet, it is referred to as food</p><p>Harold: Should I go find some more food or should I start making a shelter?</p>";
 			//change btn1
 			 document.getElementById("btn1").innerHTML = " Food";
-			document.getElementById("btn1").addEventListener("click", function() {
-				updateScene("s4");
+			 document.getElementById("btn1").setAttribute("onclick", "updateScene('s4')");
 			});
 			//change btn2
 			document.getElementById("btn2").innerHTML = "Shelter";
-			document.getElementById("btn2").addEventListener("click", function() {
-				updateScene("s5");
+			 document.getElementById("btn1").setAttribute("onclick", "updateScene('s5')");
 			});
 			break;
 		case"s3":
 			document.getElementById("storyline").innerHTML ="<p>Base: How many moons are there on that planet? How long is each day?</p><p>Harold: There are about seven moons and it takes about 37 hours to get through one day. The planet has no living beings except for some scattered plants.</p><p>Base: How's your food supply?</p><p>Harold: I am surviving only only on a few berries and mangoes right now, but I am starting to lose my supply of it.</p><p>Harold: I'll get more food or I'll go creater a shelter</p>";
 			//change btn1
 			document.getElementById("btn1").innerHTML = "Food";
-			document.getElementById("btn1").addEventListener("click", function() {
-				updateScene("s4");
+			 document.getElementById("btn1").setAttribute("onclick", "updateScene('s4')");
 			});
 			//change btn2
 			document.getElementById("btn2").innerHTML = "Shelter";
-			document.getElementById("btn2").addEventListener("click", function() {
-				updateScene("s5");
+			 document.getElementById("btn1").setAttribute("onclick", "updateScene('s5')");
 			});
 			break;
 		case"s4":
 			document.getElementById("storyline").innerHTML ="<p>Base: Apparently, that planet has an abundace of nagoes, burp causing berries, and fart causing oranges</p><p>Harold: Ok, got it.</p><p>Base: We should be able to get there in about five Earth days.<p>Harold: After I find more food, I will build a shelter.</p>";
 			//change btn1
 			document.getElementById("btn1").innerHTML = "Food";
-			document.getElementById("btn1").addEventListener("click", function() {
-				updateScene("s4");
+			 document.getElementById("btn1").setAttribute("onclick", "updateScene('s4')");
 			});
 			//change btn2
 			document.getElementById("btn2").innerHTML = "Shelter";
-			document.getElementById("btn2").addEventListener("click", function() {
-				updateScene("s5");
+			 document.getElementById("btn1").setAttribute("onclick", "updateScene('s5')");
 			});
 			break;
 		case"s5":
 			document.getElementById("storyline").innerHTML ="<p>Harold: I'll go cut some wood and prepare for the night. At night, the weather goes down to about -4 degrees Fahrenheit.</p><p>Base: That planet has a lot of Isotope Base 189 which is an extremely good insulator.</p><p>Harold: Got it</p><p>Base: Now you should go to sleep or you could stay awake and look out for more stuff during the night.</p>"; 
 			//change btn1
 			document.getElementById("btn1").innerHTML = "Stay Awake";
-			document.getElementById("btn1").addEventListener("click", function() {
-				updateScene("s7");
+			 document.getElementById("btn1").setAttribute("onclick", "updateScene('7')");
 			});
 			//change btn2
 			document.getElementById("btn2").innerHTML = "Sleep";
-			document.getElementById("btn2").addEventListener("click", function() {
-				updateScene("s6");
+			 document.getElementById("btn1").setAttribute("onclick", "updateScene('s6')");
 			});
 			break;
 		case"s6":
 			document.getElementById("storyline").innerHTML="<p>Harold: Ok now it is the morning currently unaware of the time.</p><p>Base: I guess we know that it is the morning and that you got to start doing something.</p><p>Harold:Alright I guess so... I'm going to go hunt for some meat now that I know that I have a steady flow of fruits.</p><p>Base: All right, do you want to fish or hunt on land?</p>";
 			//change btn1
 			document.getElementById("btn1").innerHTML = "Fish";
-			document.getElementById("btn1").addEventListener("click", function() {
-				updateScene("s8");
+			 document.getElementById("btn1").setAttribute("onclick", "updateScene('s8')");
 			});
 			//change btn2
 			document.getElementById("btn2").innerHTML = "Hunt";
-			document.getElementById("btn2").addEventListener("click", function() {
-				updateScene("s9");
+			 document.getElementById("btn1").setAttribute("onclick", "updateScene('s9')");
 			});
 			break;
 		case"s7":
 			document.getElementById("storyline").innerHTML="<p>You are staying awake</p><p>Harold: Good thing I started up a fire so that I wouldnt't be cold...</p><p>Alright, now if you are awake, you should at least go hunting with darkness at your advantage</p><p>Harold:Alright, should I go fish or hunt on land?</p>";
 			//change btn1
 			document.getElementById("btn1").innerHTML = "Fish ";
-			document.getElementById("btn1").addEventListener("click", function() {
-				updateScene("s8");
-			});
+			 document.getElementById("btn1").setAttribute("onclick", "updateScene('s8')");			
+			 });
 			//change btn2
 			document.getElementById("btn2").innerHTML = "Hunt";
 			document.getElementById("btn2").addEventListener("click", function() {
@@ -211,7 +195,7 @@ function updateScene(s) {
 	case"s15":
 			   document.getElementById("storyline").innerHTML="<p>Harold:Alright I'm hitting and running away from the dogs becasue they became extremely hostile towards me.... quite frankly want to execute me</p><p> Harold: I'm putting on my suit, on my way to the end of the bubble</p> Harold: Oh jeez I'm sprinting to a huge gaping hole and here goes nothing.... AHHHHHHHHH...ZZZZZZZZZZZ</p>";
 			   
-			    document.getElementById("btn1").innerHTML = "Start swimming ";
+			    document.getElementById("btn1").innerHTML = "Start swimming";
 			document.getElementById("btn1").addEventListener("click", function() {
 				updateScene("s18");
 			});
@@ -250,15 +234,15 @@ function updateScene(s) {
 			});
 			break;
 			
-	case "s18": 
-	     document.getElemntById("storyline").innerHTML=" <p></p>";
+	case"s18": 
+	     document.getElemntById("storyline").innerHTML=" <p>Harold: The dogs are following me to the surface even though I am swimming as fast as I can!</p><p> Base: We are arriving and we have your pinpoint position</p> <p> Harold: About time that you are to come because I was about to die.</p> <p> Base: Problem is though that we cant get too close to the ground because the planet has too much of a montainous landscape.</p></p>";
 		 
-		  document.getElementById("btn1").innerHTML = " ";
+		  document.getElementById("btn1").innerHTML = " Meet at surface of water";
 			document.getElementById("btn1").addEventListener("click", function() {
 				updateScene("s15");
 			});
 			//change btn2
-			document.getElementById("btn2").innerHTML = "Harold: The dogs are following me to the surface even though I am swimming as fast as I can!</p><p> Base: We are arriving and we have your pinpoint position</p> <p> Harold: About time that you are to come because I was about to die.</p> <p> Base: Problem is though that we cant get too close to the ground because the planet has too much of a montainous landscape.</p>";
+			document.getElementById("btn2").innerHTML = " Go on a hill";
 			document.getElementById("btn2").addEventListener("click", function() {
 				updateScene("s16");
 			});
